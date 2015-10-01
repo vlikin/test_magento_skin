0a61b3f1677b565b5f2c0d16450700516919dc71
Viktor Likin <viktor.likin@gmail.com>
2015-10-01 10:32:44 +0300

    Study Magento. The widgets: Youtube, Bestsold have been added.

---
+++ app/code/local/Vl/Worker/Block/Bestsold.php
@@ -0,0 +1,43 @@
+<?php
+
+class Vl_Worker_Block_Bestsold extends Mage_Core_Block_Template implements Mage_Widget_Block_Interface {
+
+
+    public function getBestsellerProducts()
+    {
+        $storeId = (int) Mage::app()->getStore()->getId();
+
+        // Date
+        $date = new Zend_Date();
+        $toDate = $date->setDay(1)->getDate()->get('Y-MM-dd');
+        $fromDate = $date->subMonth(1)->getDate()->get('Y-MM-dd');
+
+        $collection = Mage::getResourceModel('catalog/product_collection')
+            ->addAttributeToSelect(Mage::getSingleton('catalog/config')->getProductAttributes())
+            ->addStoreFilter()
+            ->addPriceData()
+            ->addTaxPercents()
+            ->addUrlRewrite()
+            ->setPageSize(6);
+
+        $collection->getSelect()
+            ->joinLeft(
+                array('aggregation' => $collection->getResource()->getTable('sales/bestsellers_aggregated_monthly')),
+                "e.entity_id = aggregation.product_id AND aggregation.store_id={$storeId} AND aggregation.period BETWEEN '{$fromDate}' AND '{$toDate}'",
+                array('SUM(aggregation.qty_ordered) AS sold_quantity')
+            )
+            ->group('e.entity_id')
+            ->order(array('sold_quantity DESC', 'e.created_at'));
+
+        Mage::getSingleton('catalog/product_status')->addVisibleFilterToCollection($collection);
+        Mage::getSingleton('catalog/product_visibility')->addVisibleInCatalogFilterToCollection($collection);
+
+        return $collection;
+    }
+
+    public function fetchView($fileName) {
+        return parent::fetchView($fileName);
+    }
+
+
+}
--- app/code/local/Vl/Worker/Block/Youtube.php
+++ app/code/local/Vl/Worker/Block/Youtube.php
@@ -1,11 +1,4 @@
 <?php
 
 class Vl_Worker_Block_Youtube extends Mage_Core_Block_Template implements Mage_Widget_Block_Interface {
-
-    protected function _11toHtml() {
-        $video_id = $this->getVideoId();
-
-        $html = '<iframe width="420" height="315" src="https://www.youtube.com/embed/' . $video_id . '" frameborder="0" allowfullscreen></iframe>';
-        return $html;
-    }
 }
